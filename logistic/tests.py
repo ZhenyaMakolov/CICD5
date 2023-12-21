1d5723d9e8405c0d42f0a75d5977f7e411a8c6cd
Zhenya Makolov <emakolov@bk.ru>
2023-12-21 06:02:41 +0300

    Add cicd pipeline

---
+++ logistic/tests.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+from rest_framework.test import APIClient
+
+class TestSomething(TestCase):
+    def test_sample_view(self):
+        client = APIClient()
+        URL = '/api/v1/test/'
+        response = client.get(URL)
+        assert response.status_code == 200
